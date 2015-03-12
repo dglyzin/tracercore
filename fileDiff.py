dbb701070888e03008140118f5124c72173f44ef
Daniil Frolov <devnull@localhost>
2015-03-12 11:36:58 +0300

    Корректный commit про скрипт сравнения файлов

---
+++ fileDiff.py
@@ -0,0 +1,11 @@
+import sys
+import filecmp
+
+argc = len(sys.argv)
+
+for i in range(1, argc - 1):
+	for j in range(i + 1, argc):
+		if filecmp.cmp(sys.argv[i], sys.argv[j], shallow=False):
+			print "Test OK! " + sys.argv[i] + " " + sys.argv[j]
+		else:
+			print "TEST FAILED! " + sys.argv[i] + " " + sys.argv[j]
