b1c570451960ed1280990a4548b73d4c74f28d17
dglyzin <devnull@localhost>
2015-03-16 19:50:34 +0300

    bugrun дает результаты по времени, не соответствующие реальному времени

--- bugrun1.sh
+++ bugrun1.sh
@@ -1,4 +1,6 @@
 rm res_2400
 rm stat_2400
-python run.py srun 2 test_2400x2400_8 res_2400 -stat stat_2400
-python run.py srun 3 test_2400x2400_8 res_2400 -stat stat_2400
+python run.py srun 2 test_2400x2400_8 res_2400 -step 1000 -stat stat_2400
+python run.py srun 3 test_2400x2400_8 res_2400 -step 1000 -stat stat_2400 
+python run.py srun 2 test_2400x2400_8 res_2400   -stat stat_2400
+python run.py srun 3 test_2400x2400_8 res_2400   -stat stat_2400
