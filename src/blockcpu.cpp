a6800dc95f988be082df0940f91aeed2333867d9
Daniil Frolov <devnull@localhost>
2015-04-16 17:44:06 +0300

    Реализован расчет границы для трехмерного случая

--- src/blockcpu.cpp
+++ src/blockcpu.cpp
@@ -315,7 +315,87 @@ void BlockCpu::computeStageBorder_2d(int stage, double time, double step) {
 }
 
 void BlockCpu::computeStageBorder_3d(int stage, double time, double step) {
+# pragma omp parallel
+	{
+		double* result = mSolver->getStageResult(stage);
+		double* source = mSolver->getStageSource(stage);
+# pragma omp for
+		for (int z = 0; z < haloSize; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = 0; y < yCount; ++y) {
+				int yShift = xCount * y;
+				for (int x = 0; x < xCount; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
 
+# pragma omp for
+		for (int z = zCount - haloSize; z < zCount; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = 0; y < yCount; ++y) {
+				int yShift = xCount * y;
+				for (int x = 0; x < xCount; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
+
+# pragma omp for
+		for (int z = haloSize; z < zCount - haloSize; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = 0; y < haloSize; ++y) {
+				int yShift = xCount * y;
+				for (int x = 0; x < xCount; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
+
+# pragma omp for
+		for (int z = haloSize; z < zCount - haloSize; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = yCount - haloSize; y < yCount; ++y) {
+				int yShift = xCount * y;
+				for (int x = 0; x < xCount; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
+
+# pragma omp for
+		for (int z = haloSize; z < zCount - haloSize; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = haloSize; y < yCount - haloSize; ++y) {
+				int yShift = xCount * y;
+				for (int x = 0; x < haloSize; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
+
+# pragma omp for
+		for (int z = haloSize; z < zCount - haloSize; ++z) {
+			int zShift = yCount * xCount * z;
+			for (int y = haloSize; y < yCount - haloSize; ++y) {
+				int yShift = xCount * y;
+				for (int x = xCount - haloSize; x < xCount; ++x) {
+					int xShift = x;
+					cout << "Calc z_" << z << " y_" << y << " x_" << x << endl;
+					mUserFuncs[ mCompFuncNumber[ zShift + yShift + xShift ] ](result, source, time, x, y, z, mParams, externalBorder);
+				}
+			}
+		}
 }
 
 void BlockCpu::prepareStageData(int stage) {
