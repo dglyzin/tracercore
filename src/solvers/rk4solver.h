4c0c643d4537a39faecab2e1f06b16ff76c24b41
Daniil Frolov <devnull@localhost>
2015-04-28 16:43:36 +0300

    Add RK4Solver

---
+++ src/solvers/rk4solver.h
@@ -0,0 +1,40 @@
+/*
+ * rk4solver.h
+ *
+ *  Created on: 28 апр. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_SOLVERS_RK4SOLVER_H_
+#define SRC_SOLVERS_RK4SOLVER_H_
+
+#include "solver.h"
+
+class RK4Solver: public Solver {
+public:
+public:
+	RK4Solver(int _mCount);
+	~RK4Solver();
+
+    virtual void copyState(double* result) { return; }
+
+	double* getStageSource(int stage);
+	double* getStageResult(int stage);
+
+	double getStageTimeStep(int stage);
+
+	void prepareArgument(int stage, double timeStep) { return; }
+
+	void confirmStep(double timestep);
+
+	double getStepError(double timeStep, double aTol, double rTol) { return 0.0; }
+
+protected:
+    double* mTempStore1;
+    double* mTempStore2;
+    double* mTempStore3;
+    double* mTempStore4;
+    double* mArg;
+};
+
+#endif /* SRC_SOLVERS_RK4SOLVER_H_ */
