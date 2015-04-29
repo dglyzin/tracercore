8a7dfd384db5eeb4e92b6bbb6ff1a962a5eaf203
Daniil Frolov <devnull@localhost>
2015-04-29 11:35:50 +0300

    Add DP45Solver

---
+++ src/solvers/dp45solver.cpp
@@ -0,0 +1,62 @@
+/*
+ * dp45solver.cpp
+ *
+ *  Created on: 29 апр. 2015 г.
+ *      Author: frolov
+ */
+
+#include "dp45solver.h"
+
+DP45Solver::DP45Solver() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DP45Solver::~DP45Solver() {
+	// TODO Auto-generated destructor stub
+}
+
+double* DP45Solver::getStageSource(int stage){
+	if      (stage == 0) return mArg;
+	else if (stage == 1) return mArg;
+	else if (stage == 2) return mArg;
+	else if (stage == 3) return mArg;
+	else if (stage == 4) return mArg;
+	else if (stage == 5) return mArg;
+	else if (stage == -1) return mState;
+	else assert(0);
+	return NULL;
+}
+
+double* DP45Solver::getStageResult(int stage){
+	if      (stage == 0) return mTempStore2;
+	else if (stage == 1) return mTempStore3;
+	else if (stage == 2) return mTempStore4;
+	else if (stage == 3) return mTempStore5;
+	else if (stage == 4) return mTempStore6;
+	else if (stage == 5) return mTempStore7;
+	else if (stage == -1) return mTempStore1;
+	else assert(0);
+	return NULL;
+}
+
+double DP45Solver::getStageTimeStep(int stage){
+    if      (stage == 0) return c2;
+    else if (stage == 1) return c3;
+    else if (stage == 2) return c4;
+    else if (stage == 3) return c5;
+    else if (stage == 4) return 1.0;
+    else if (stage == 5) return 1.0;
+    else if (stage ==-1) return 0.0;
+    else assert(0);
+    return 0.0;
+}
+
+/*void DP45Solver::confirmStep(double timestep){
+    double* temp = mState;
+    mState = mArg;
+    mArg = temp;
+#pragma omp parallel for
+	for (int idx=0; idx<mCount; idx++)
+		mArg[idx] = mState[idx]+a21*timestep*mTempStore7[idx];
+}*/
---
+++ src/solvers/dp45solver.h
@@ -0,0 +1,57 @@
+/*
+ * dp45solver.h
+ *
+ *  Created on: 29 апр. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_SOLVERS_DP45SOLVER_H_
+#define SRC_SOLVERS_DP45SOLVER_H_
+
+#include "solver.h"
+
+class DP45Solver: public Solver {
+public:
+	DP45Solver();
+	virtual ~DP45Solver();
+
+	virtual void copyState(double* result) { return; }
+
+	double* getStageSource(int stage);
+	double* getStageResult(int stage);
+
+	virtual double getStageTimeStep(int stage);
+
+	void prepareArgument(int stage, double timeStep) { return; }
+
+	virtual void confirmStep(double timestep) { return; }
+
+	virtual double getStepError(double timeStep, double aTol, double rTol) { return 0.0; }
+
+private:
+    double* mTempStore1;
+    double* mTempStore2;
+    double* mTempStore3;
+    double* mTempStore4;
+    double* mTempStore5;
+    double* mTempStore6;
+    double* mTempStore7;
+    double* mArg;
+
+
+    const double c2=0.2, c3=0.3, c4=0.8, c5=8.0/9.0;
+
+    const double a21=0.2, a31=3.0/40.0, a32=9.0/40.0;
+    const double a41=44.0/45.0, a42=-56.0/15.0, a43=32.0/9.0;
+    const double a51=19372.0/6561.0, a52=-25360.0/2187.0;
+    const double a53=64448.0/6561.0, a54=-212.0/729.0;
+    const double a61=9017.0/3168.0, a62=-355.0/33.0, a63=46732.0/5247.0;
+    const double a64=49.0/176.0, a65=-5103.0/18656.0;
+    const double a71=35.0/384.0, a73=500.0/1113.0, a74=125.0/192.0;
+    const double a75=-2187.0/6784.0, a76=11.0/84.0;
+    const double e1=71.0/57600.0, e3=-71.0/16695.0, e4=71.0/1920.0;
+    const double e5=-17253.0/339200.0, e6=22.0/525.0, e7=-1.0/40.0;
+    const double facmin=0.5, facmax = 2, fac = 0.9;
+};
+
+#endif /* SRC_SOLVERS_DP45SOLVER_H_ */
