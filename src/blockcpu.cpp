7451e57535bbe7d64ce832303358fda1bcd653ea
dglyzin <devnull@localhost>
2015-05-27 17:26:22 +0300

    11

--- src/blockcpu.cpp
+++ src/blockcpu.cpp
@@ -54,28 +54,7 @@ BlockCpu::BlockCpu(int _dimension, int _xCount, int _yCount, int _zCount,
 	double* matrix = mSolver->getStatePtr();
 	mUserInitFuncs[0](matrix,_initFuncNumber);
 	cout << "Initial values filled \n";
-    /*
-	for (int i = 0; i < zCount; ++i) {
-			int zShift = xCount * yCount * i;
 
-			for (int j = 0; j < yCount; ++j) {
-				int yShift = xCount * j;
-
-				for (int k = 0; k < xCount; ++k) {
-					int xShift = k;
-                    printf("(");
-					for (int l = 0; l < cellSize; ++l) {
-						int cellShift = l;
-
-						printf("%f ", matrix[ (zShift + yShift + xShift)*cellSize + cellShift ]);
-
-					}
-					printf(") ");
-				}
-				printf("\n");
-			}
-			printf("\n");
-		}*/
 }
 
 BlockCpu::~BlockCpu() {
