17f2713c6207772460ed8c9b5fa87c865ae0747d
Daniil Frolov <devnull@localhost>
2015-08-28 14:49:22 +0300

    Компилируется вместе с новыми классами

--- src/blocks/blockcpu1d.h
+++ src/blocks/blockcpu1d.h
@@ -12,7 +12,13 @@
 
 class BlockCpu1d: public BlockCpu {
 public:
-	BlockCpu1d();
+	BlockCpu1d(int _blockNumber, int _dimension, int _xCount, int _yCount, int _zCount,
+			int _xOffset, int _yOffset, int _zOffset,
+			int _nodeNumber, int _deviceNumber,
+			int _haloSize, int _cellSize,
+			unsigned short int* _initFuncNumber, unsigned short int* _compFuncNumber,
+			int _solverIdx, double _aTol, double _rTol);
+
 	virtual ~BlockCpu1d();
 };
 
--- src/blocks/blockcpu2d.h
+++ src/blocks/blockcpu2d.h
@@ -12,7 +12,13 @@
 
 class BlockCpu2d: public BlockCpu {
 public:
-	BlockCpu2d();
+	BlockCpu2d(int _blockNumber, int _dimension, int _xCount, int _yCount, int _zCount,
+			int _xOffset, int _yOffset, int _zOffset,
+			int _nodeNumber, int _deviceNumber,
+			int _haloSize, int _cellSize,
+			unsigned short int* _initFuncNumber, unsigned short int* _compFuncNumber,
+			int _solverIdx, double _aTol, double _rTol);
+
 	virtual ~BlockCpu2d();
 };
 
