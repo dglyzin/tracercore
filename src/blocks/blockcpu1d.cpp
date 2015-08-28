bf2e145bd703f7fc3503ae9a8131e3480a608ac4
Daniil Frolov <devnull@localhost>
2015-08-28 14:32:46 +0300

    Добавлен класс для реализации одномерного случая на центальном процессоре

---
+++ src/blocks/blockcpu1d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockcpu1d.cpp
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockcpu1d.h"
+
+BlockCpu1d::BlockCpu1d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockCpu1d::~BlockCpu1d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockcpu1d.h
@@ -0,0 +1,19 @@
+/*
+ * blockcpu1d.h
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKCPU1D_H_
+#define SRC_BLOCKS_BLOCKCPU1D_H_
+
+#include "blockcpu.h"
+
+class BlockCpu1d: public BlockCpu {
+public:
+	BlockCpu1d();
+	virtual ~BlockCpu1d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKCPU1D_H_ */
