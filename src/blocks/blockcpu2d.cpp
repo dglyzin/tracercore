90dc04cb523d2cc8d70b451ccb9ad0293c26fcd5
Daniil Frolov <devnull@localhost>
2015-08-28 14:33:00 +0300

    Добавлен класс для реализации двемерного случая на центальном процессоре

---
+++ src/blocks/blockcpu2d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockcpu2d.cpp
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockcpu2d.h"
+
+BlockCpu2d::BlockCpu2d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockCpu2d::~BlockCpu2d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockcpu2d.h
@@ -0,0 +1,19 @@
+/*
+ * blockcpu2d.h
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKCPU2D_H_
+#define SRC_BLOCKS_BLOCKCPU2D_H_
+
+#include "blockcpu.h"
+
+class BlockCpu2d: public BlockCpu {
+public:
+	BlockCpu2d();
+	virtual ~BlockCpu2d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKCPU2D_H_ */
