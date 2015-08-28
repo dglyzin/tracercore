fe2b8de383454d9f311fb78d86d14a9dda9520fa
Daniil Frolov <devnull@localhost>
2015-08-28 14:33:14 +0300

    Добавлен класс для реализации трехмерного случая на центальном процессоре

---
+++ src/blocks/blockcpu3d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockcpu3d.cpp
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockcpu3d.h"
+
+BlockCpu3d::BlockCpu3d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockCpu3d::~BlockCpu3d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockcpu3d.h
@@ -0,0 +1,19 @@
+/*
+ * blockcpu3d.h
+ *
+ *  Created on: 28 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKCPU3D_H_
+#define SRC_BLOCKS_BLOCKCPU3D_H_
+
+#include "blockcpu.h"
+
+class BlockCpu3d: public BlockCpu {
+public:
+	BlockCpu3d();
+	virtual ~BlockCpu3d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKCPU3D_H_ */
