bbc02666aa08a5bbcdd68a7710a0b82ea9a2b2e5
Daniil Frolov <devnull@localhost>
2015-08-29 10:45:57 +0300

    Добавлен класс для реализации трехмерного случая на видеокарте

---
+++ src/blocks/blockgpu3d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockgpu3d.cpp
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockgpu3d.h"
+
+BlockGpu3d::BlockGpu3d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockGpu3d::~BlockGpu3d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockgpu3d.h
@@ -0,0 +1,19 @@
+/*
+ * blockgpu3d.h
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKGPU3D_H_
+#define SRC_BLOCKS_BLOCKGPU3D_H_
+
+#include "blockgpu.h"
+
+class BlockGpu3d: public BlockGpu {
+public:
+	BlockGpu3d();
+	virtual ~BlockGpu3d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKGPU3D_H_ */
