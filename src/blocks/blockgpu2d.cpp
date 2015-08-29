e82e8f037384c716217644e36f7d4b7483388a21
Daniil Frolov <devnull@localhost>
2015-08-29 10:45:43 +0300

    Добавлен класс для реализации двумерного случая на видеокарте

---
+++ src/blocks/blockgpu2d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockgpu2d.cpp
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockgpu2d.h"
+
+BlockGpu2d::BlockGpu2d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockGpu2d::~BlockGpu2d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockgpu2d.h
@@ -0,0 +1,19 @@
+/*
+ * blockgpu2d.h
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKGPU2D_H_
+#define SRC_BLOCKS_BLOCKGPU2D_H_
+
+#include "blockgpu.h"
+
+class BlockGpu2d: public BlockGpu {
+public:
+	BlockGpu2d();
+	virtual ~BlockGpu2d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKGPU2D_H_ */
