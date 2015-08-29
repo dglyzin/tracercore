54344c42870f03d1a5fd8e4eeb319b040decbf87
Daniil Frolov <devnull@localhost>
2015-08-29 10:45:32 +0300

    Добавлен класс для реализации одномерного случая на видеокарте

---
+++ src/blocks/blockgpu1d.cpp
@@ -0,0 +1,18 @@
+/*
+ * blockgpu1d.cpp
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#include "blockgpu1d.h"
+
+BlockGpu1d::BlockGpu1d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockGpu1d::~BlockGpu1d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/blocks/blockgpu1d.h
@@ -0,0 +1,19 @@
+/*
+ * blockgpu1d.h
+ *
+ *  Created on: 29 авг. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_BLOCKS_BLOCKGPU1D_H_
+#define SRC_BLOCKS_BLOCKGPU1D_H_
+
+#include "blockgpu.h"
+
+class BlockGpu1d: public BlockGpu {
+public:
+	BlockGpu1d();
+	virtual ~BlockGpu1d();
+};
+
+#endif /* SRC_BLOCKS_BLOCKGPU1D_H_ */
