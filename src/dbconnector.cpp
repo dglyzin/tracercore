16289d220b6ce984192dba63b7a5bd56efc67ac8
Dmitry Glyzin <glyzin@gmail.com>
2015-09-02 14:51:14 +0300

    connector fixed

--- src/dbconnector.cpp
+++ src/dbconnector.cpp
@@ -4,7 +4,7 @@
  *  Created on: Sep 2, 2015
  *      Author: dglyzin
  */
-
+#include "enums.h"
 #include <stdlib.h>
 #include <iostream>
 #include <cstdio>
@@ -47,58 +47,19 @@ void execSQL(char *sqlString){
 
 
 void dbConnSetJobState(int jobId, int state){
-	try {
-	  sql::Driver *driver;
-	  sql::Connection *con;
-	  sql::Statement *stmt;
+	char stmtstring[256];
+	if (state == JS_FINISHED)
+	    sprintf(stmtstring, "UPDATE jobs SET state=%d, finishtime=NOW() WHERE id=%d", state, jobId);
+	else
+	    sprintf(stmtstring, "UPDATE jobs SET state=%d WHERE id=%d", state, jobId);
 
-	  /* Create a connection */
-	  driver = get_driver_instance();
-	  con = driver->connect("192.168.10.100", "cherry", "sho0ro0p");
-	  /* Connect to the MySQL test database */
-	  con->setSchema("cluster");
-	  char stmtstring[256];
-	  //change state to computing
-	  sprintf(stmtstring, "UPDATE jobs SET state=%d WHERE id=%d", state, jobId);
-	  stmt = con->createStatement();
-	  stmt->execute(stmtstring);
-	  delete stmt;
-	  delete con;
-	} catch (sql::SQLException &e) {
-	  cout << "# ERR: SQLException in " << __FILE__;
-	  cout << "(" << __FUNCTION__ << ") on line "   << __LINE__ << endl;
-	  cout << "# ERR: " << e.what();
-	  cout << " (MySQL error code: " << e.getErrorCode();
-	  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
-	}
-
-
+	execSQL(stmtstring);
 }
 
 void dbConnSetJobPercentage(int jobId, int percentage){
-	try {
-	  sql::Driver *driver;
-	  sql::Connection *con;
-	  sql::Statement *stmt;
-	  /* Create a connection */
-	  driver = get_driver_instance();
-	  con = driver->connect("192.168.10.100", "cherry", "sho0ro0p");
-	  /* Connect to the MySQL test database */
-	  con->setSchema("cluster");
-	  char stmtstring[256];
-	  sprintf(stmtstring, "UPDATE jobs SET percentage=%d WHERE id=%d", percentage, jobId);
-	  stmt = con->createStatement();
-	  stmt->execute(stmtstring);
-	  delete stmt;
-	  delete con;
-	} catch (sql::SQLException &e) {
-	  cout << "# ERR: SQLException in " << __FILE__;
-	  cout << "(" << __FUNCTION__ << ") on line "   << __LINE__ << endl;
-	  cout << "# ERR: " << e.what();
-	  cout << " (MySQL error code: " << e.getErrorCode();
-	  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
-	}
-
+	char stmtstring[256];
+	sprintf(stmtstring, "UPDATE jobs SET percentage=%d WHERE id=%d", percentage, jobId);
+	execSQL(stmtstring);
 }
 
 
