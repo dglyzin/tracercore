a26bc8c4ce3a92bb18d789f09f330adc424554b9
Daniil Frolov <devnull@localhost>
2015-09-18 16:41:08 +0300

    CPU added

---
+++ src/processingUnit/cpu/cpu.cpp
@@ -0,0 +1,18 @@
+/*
+ * cpu.cpp
+ *
+ *  Created on: 18 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#include "cpu.h"
+
+CPU::CPU() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CPU::~CPU() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingUnit/cpu/cpu.h
@@ -0,0 +1,28 @@
+/*
+ * cpu.h
+ *
+ *  Created on: 18 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_CPU_CPU_H_
+#define SRC_PROCESSINGUNIT_CPU_CPU_H_
+
+class CPU: public ProcessingUnit {
+public:
+	CPU();
+	virtual ~CPU();
+
+	virtual void computeBorder(double* result, double** source, double time, double* parametrs, double** externalBorder) = 0;
+	virtual void computeCenter(double* result, double** source, double time, double* parametrs, double** externalBorder) = 0;
+
+	void prepareBorder(double* result, double* source, int zStart, int zStop, int yStart, int yStop, int xStart, int xStop);
+
+	double* newDoubleArray(int size);
+	double** newDoublePointerArray(int size);
+
+	int* newIntArray(int size);
+	int** newIntPointerArray(int size);
+};
+
+#endif /* SRC_PROCESSINGUNIT_CPU_CPU_H_ */
