f9015b67c9699b4211d9190dd716535545667ed2
Daniil Frolov <devnull@localhost>
2015-09-22 15:17:02 +0300

    Часть реализации. Выделение и удаление памяти, общей для ЦПУ и видеокарты

--- src/processingUnit/processingunit.cpp
+++ src/processingUnit/processingunit.cpp
@@ -16,3 +16,24 @@ ProcessingUnit::~ProcessingUnit() {
 	// TODO Auto-generated destructor stub
 }
 
+double* ProcessingUnit::newDoublePinnedArray(int size) {
+	double* array;
+
+	array = getDoublePinnedArray(size);
+
+	doublePinnedArrays.push_back(array);
+
+	return array;
+}
+
+double* ProcessingUnit::getDoublePinnedArray(int size) {
+	double* array;
+
+	cudaMallocHost ( (void**)&array, size * sizeof(double) );
+
+	return array;
+}
+
+void ProcessingUnit::deleteDoublePinnedArray(double* toDelete) {
+	cudaFreeHost(toDelete);
+}
