8a86ea71a4348df983e032e65df2792f7abe8a69
Daniil Frolov <devnull@localhost>
2015-09-22 15:16:29 +0300

    Описание интерфейса выделения памяти на вычислительном устройстве

--- src/processingUnit/processingunit.h
+++ src/processingUnit/processingunit.h
@@ -8,6 +8,9 @@
 #ifndef SRC_PROCESSINGUNIT_PROCESSINGUNIT_H_
 #define SRC_PROCESSINGUNIT_PROCESSINGUNIT_H_
 
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
 #include <list>
 
 class ProcessingUnit {
@@ -20,11 +23,38 @@ public:
 
 	virtual void prepareBorder(double* result, double* source, int zStart, int zStop, int yStart, int yStop, int xStart, int xStop) = 0;
 
-	virtual double* newDoubleArray(int size) = 0;
-	virtual double** newDoublePointerArray(int size) = 0;
+	double* newDoubleArray(int size);
+	double** newDoublePointerArray(int size);
 
-	virtual int* newIntArray(int size) = 0;
-	virtual int** newIntPointerArray(int size) = 0;
+	int* newIntArray(int size);
+	int** newIntPointerArray(int size);
+
+	double* newDoublePinnedArray(int size);
+
+private:
+	std::list<double*> doubleArrays;
+	std::list<double**> doublePointerArrays;
+
+	std::list<int*> intArrays;
+	std::list<int**> intPointerArrays;
+
+	std::list<double*> doublePinnedArrays;
+
+	virtual double* getDoubleArray(int size) = 0;
+	virtual double** getDoublePointerArray(int size) = 0;
+
+	virtual int* getIntArray(int size) = 0;
+	virtual int** getIntPointerArray(int size) = 0;
+
+	double* getDoublePinnedArray(int size);
+
+	virtual void deleteIntArray(int* toDelete) = 0;
+	virtual void deleteIntPointerArray(int** toDelete) = 0;
+
+	virtual void deleteDoubleArray(double* toDelete) = 0;
+	virtual void deleteDoublePointerArray(double** toDelete) = 0;
+
+	void deleteDoublePinnedArray(double* toDelete);
 };
 
 #endif /* SRC_PROCESSINGUNIT_PROCESSINGUNIT_H_ */
