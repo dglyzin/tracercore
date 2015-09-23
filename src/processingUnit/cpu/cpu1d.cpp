acd19b7adc3b1ec33a972647918fd97a622bd0ef
Daniil Frolov <devnull@localhost>
2015-09-23 13:39:10 +0300

    add CPU 1d

---
+++ src/processingUnit/cpu/cpu1d.cpp
@@ -0,0 +1,18 @@
+/*
+ * cpu1d.cpp
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#include "cpu1d.h"
+
+CPU_1d::CPU_1d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CPU_1d::~CPU_1d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingUnit/cpu/cpu1d.h
@@ -0,0 +1,19 @@
+/*
+ * cpu1d.h
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_CPU_CPU1D_H_
+#define SRC_PROCESSINGUNIT_CPU_CPU1D_H_
+
+#include "cpu.h"
+
+class CPU_1d: public CPU {
+public:
+	CPU_1d();
+	virtual ~CPU_1d();
+};
+#endif /* SRC_PROCESSINGUNIT_CPU_CPU1D_H_ */
+
