4986ddb6f3e852a2fe15d03aee63f63951fbb133
Daniil Frolov <devnull@localhost>
2015-09-23 13:39:33 +0300

    add CPU 3d

---
+++ src/processingUnit/cpu/cpu3d.cpp
@@ -0,0 +1,18 @@
+/*
+ * cpu3d.cpp
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#include "cpu3d.h"
+
+CPU_3d::CPU_3d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CPU_3d::~CPU_3d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingUnit/cpu/cpu3d.h
@@ -0,0 +1,19 @@
+/*
+ * cpu3d.h
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_CPU_CPU3D_H_
+#define SRC_PROCESSINGUNIT_CPU_CPU3D_H_
+
+#include "cpu.h"
+
+class CPU_3d: public CPU {
+public:
+	CPU_3d();
+	virtual ~CPU_3d();
+};
+
+#endif /* SRC_PROCESSINGUNIT_CPU_CPU3D_H_ */
