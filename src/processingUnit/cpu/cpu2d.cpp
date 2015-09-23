991abaf69f1902cba5943f94f95ecf94cd13ac03
Daniil Frolov <devnull@localhost>
2015-09-23 13:39:23 +0300

    add CPU 2d

---
+++ src/processingUnit/cpu/cpu2d.cpp
@@ -0,0 +1,18 @@
+/*
+ * cpu2d.cpp
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#include "cpu2d.h"
+
+CPU_2d::CPU_2d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CPU_2d::~CPU_2d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingUnit/cpu/cpu2d.h
@@ -0,0 +1,19 @@
+/*
+ * cpu2d.h
+ *
+ *  Created on: 23 сент. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_CPU_CPU2D_H_
+#define SRC_PROCESSINGUNIT_CPU_CPU2D_H_
+
+#include "cpu.h"
+
+class CPU_2d: public CPU {
+public:
+	CPU_2d();
+	virtual ~CPU_2d();
+};
+
+#endif /* SRC_PROCESSINGUNIT_CPU_CPU2D_H_ */
