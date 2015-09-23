b73400b0c017d1ad37a2c80e8dbe2b365b6cef10
Daniil Frolov <devnull@localhost>
2015-09-23 16:52:57 +0300

    compute for cpu 2d

--- src/processingUnit/cpu/cpu2d.cpp
+++ src/processingUnit/cpu/cpu2d.cpp
@@ -16,3 +16,66 @@ CPU_2d::~CPU_2d() {
 	// TODO Auto-generated destructor stub
 }
 
+void CPU_2d::computeBorder(double* result, double** source, double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+# pragma omp parallel
+	{
+# pragma omp for
+		for (int x = 0; x < xCount; ++x) {
+			int xShift = x;
+			for (int y = 0; y < haloSize; ++y) {
+				int yShift = xCount * y;
+				//cout << "Calc y_" << y << " x_" << x << endl;
+				//printf("Calc y = %d, x = %d\n", y, x);
+				mUserFuncs[ mCompFuncNumber[ yShift + xShift ] ](result, source, time, x, y, 0, parametrs, externalBorder);
+			}
+		}
+
+# pragma omp for
+		for (int x = 0; x < xCount; ++x) {
+			int xShift = x;
+			for (int y = yCount - haloSize; y < yCount; ++y) {
+				int yShift = xCount * y;
+				//cout << "Calc y_" << y << " x_" << x << endl;
+				//printf("Calc y = %d, x = %d\n", y, x);
+				mUserFuncs[ mCompFuncNumber[ yShift + xShift ] ](result, source, time, x, y, 0, parametrs, externalBorder);
+			}
+		}
+
+# pragma omp for
+		for (int y = haloSize; y < yCount - haloSize; ++y) {
+			int yShift = xCount * y;
+			for (int x = 0; x < haloSize; ++x) {
+				int xShift = x;
+				//cout << "Calc y_" << y << " x_" << x << endl;
+				//printf("Calc y = %d, x = %d\n", y, x);
+				mUserFuncs[ mCompFuncNumber[ yShift + xShift ] ](result, source, time, x, y, 0, parametrs, externalBorder);
+			}
+		}
+
+# pragma omp for
+		for (int y = haloSize; y < yCount - haloSize; ++y) {
+			int yShift = xCount * y;
+			for (int x = xCount - haloSize; x < xCount; ++x) {
+				int xShift = x;
+				//cout << "Calc y_" << y << " x_" << x << endl;
+				//printf("Calc y = %d, x = %d\n", y, x);
+				mUserFuncs[ mCompFuncNumber[ yShift + xShift ] ](result, source, time, x, y, 0, parametrs, externalBorder);
+			}
+		}
+	}
+}
+
+void CPU_2d::computeCenter(double* result, double** source, double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+# pragma omp parallel
+	{
+# pragma omp for
+		for (int y = haloSize; y < yCount - haloSize; ++y) {
+			int yShift = xCount * y;
+			for (int x = haloSize; x < xCount - haloSize; ++x) {
+				int xShift = x;
+				//cout << "Calc y_" << y << " x_" << x << endl;
+				mUserFuncs[ mCompFuncNumber[ yShift + xShift ] ](result, source, time, x, y, 0, parametrs, externalBorder);
+			}
+		}
+	}
+}
