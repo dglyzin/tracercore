715b7335aee3704d852241c806d12bbc3fadfc63
Daniil Frolov <devnull@localhost>
2015-10-01 14:43:57 +0300

    rk4 added

---
+++ src/stepstorage/rk4storage.cpp
@@ -0,0 +1,18 @@
+/*
+ * rk4storage.cpp
+ *
+ *  Created on: 01 окт. 2015 г.
+ *      Author: frolov
+ */
+
+#include "rk4storage.h"
+
+RK4Storage::RK4Storage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RK4Storage::~RK4Storage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/stepstorage/rk4storage.h
@@ -0,0 +1,53 @@
+/*
+ * rk4storage.h
+ *
+ *  Created on: 01 окт. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_STEPSTORAGE_RK4STORAGE_H_
+#define SRC_STEPSTORAGE_RK4STORAGE_H_
+
+#include "stepstorage.h"
+
+class RK4Storage: public StepStorage {
+public:
+	RK4Storage();
+	RK4Storage(ProcessingUnit* pu, int count, double _aTol, double _rTol);
+	virtual ~RK4Storage();
+
+    double* getStageSource(int stage);
+    double* getStageResult(int stage);
+
+    double getStageTimeStep(int stage);
+
+    void prepareArgument(ProcessingUnit* pc, int stage, double timestep);
+
+    void confirmStep(double timestep);
+    void rejectStep(double timestep);
+
+    double getStepError(double timeStep);
+
+    bool isFSAL();
+    bool isVariableStep();
+    int getStageCount();
+
+	double getNewStep(double timeStep, double error, int totalDomainElements);
+	bool isErrorPermissible(double error, int totalDomainElements);
+
+	double* getDenseOutput(StepStorage* secondState);
+
+private:
+    double* mTempStore1;
+    double* mTempStore2;
+    double* mTempStore3;
+    double* mTempStore4;
+    double* mArg;
+
+    static const double b1 = 1.0/6.0;
+    static const double b2 = 1.0/3.0;
+    static const double b3 = 1.0/3.0;
+    static const double b4 = 1.0/6.0;
+};
+
+#endif /* SRC_STEPSTORAGE_RK4STORAGE_H_ */
