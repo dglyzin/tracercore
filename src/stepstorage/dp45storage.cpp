7bf557cdfc27b23522eb5733458e9e94f3c9c895
Daniil Frolov <devnull@localhost>
2015-10-06 16:02:30 +0300

    add dp45

---
+++ src/stepstorage/dp45storage.cpp
@@ -0,0 +1,18 @@
+/*
+ * dp45storage.cpp
+ *
+ *  Created on: 06 окт. 2015 г.
+ *      Author: frolov
+ */
+
+#include "dp45storage.h"
+
+DP45Storage::DP45Storage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DP45Storage::~DP45Storage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/stepstorage/dp45storage.h
@@ -0,0 +1,19 @@
+/*
+ * dp45storage.h
+ *
+ *  Created on: 06 окт. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_STEPSTORAGE_DP45STORAGE_H_
+#define SRC_STEPSTORAGE_DP45STORAGE_H_
+
+#include "stepstorage.h"
+
+class DP45Storage: public StepStorage {
+public:
+	DP45Storage();
+	virtual ~DP45Storage();
+};
+
+#endif /* SRC_STEPSTORAGE_DP45STORAGE_H_ */
