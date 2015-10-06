a5af80ebeb389a2fef0c7ee2a0e9d9b8adcea605
Daniil Frolov <devnull@localhost>
2015-10-06 16:58:11 +0300

    ***

--- src/stepstorage/dp45storage.cpp
+++ src/stepstorage/dp45storage.cpp
@@ -57,3 +57,71 @@ double* DP45Storage::getStageSource(int stage) {
 			return NULL;
 	}
 }
+
+double* DP45Storage::getStageResult(int stage) {
+	/*if      (stage == 0) return mTempStore2;
+	else if (stage == 1) return mTempStore3;
+	else if (stage == 2) return mTempStore4;
+	else if (stage == 3) return mTempStore5;
+	else if (stage == 4) return mTempStore6;
+	else if (stage == 5) return mTempStore7;
+	else if (stage == -1) return mTempStore1;
+	else assert(0);
+	return NULL;*/
+
+	switch (stage) {
+		case 0:
+			return mTempStore2;
+		case 1:
+			return mTempStore3;
+		case 2:
+			return mTempStore4;
+		case 3:
+			return mTempStore5;
+		case 4:
+			return mTempStore6;
+		case 5:
+			return mTempStore7;
+		case -1:
+			return mTempStore1;
+		default:
+			assert(0);
+			return NULL;
+	}
+}
+
+double DP45Storage::getStageTimeStep(int stage) {
+	/*if      (stage == 0) return c2;
+	else if (stage == 1) return c3;
+	else if (stage == 2) return c4;
+	else if (stage == 3) return c5;
+	else if (stage == 4) return 1.0;
+	else if (stage == 5) return 1.0;
+	else if (stage ==-1) return 0.0;
+	else assert(0);
+	return 0.0;*/
+
+	switch (stage) {
+		case 0:
+			return c2;
+		case 1:
+			return c3;
+		case 2:
+			return c4;
+		case 3:
+			return c5;
+		case 4:
+			return 1.0;
+		case 5:
+			return 1.0;
+		case -1:
+			return 0.0;
+		default:
+			assert(0);
+			return 0.0;
+	}
+}
+
+void DP45Storage::prepareArgument(ProcessingUnit* pc, int stage, double timestep) {
+
+}
