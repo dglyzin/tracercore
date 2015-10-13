864fbf0333d606fdcacc0c4bab54eeb070f3ca7f
Daniil Frolov <devnull@localhost>
2015-10-13 17:11:49 +0300

    ***

--- src/blocks/block.h
+++ src/blocks/block.h
@@ -8,6 +8,8 @@
 #ifndef SRC_BLOCKS_BLOCK_H_
 #define SRC_BLOCKS_BLOCK_H_
 
+#include <vector>
+
 #include "../problem/ordinary.h"
 
 #include "../processingunit/processingunit.h"
@@ -30,6 +32,38 @@ protected:
 	ProblemType* problem;
 
 
+	int dimension;
+
+	int xCount;
+	int yCount;
+	int zCount;
+
+	int xOffset;
+	int yOffset;
+	int zOffset;
+
+
+	int* sendBorderInfo;
+	std::vector<int> tempSendBorderInfo;
+
+	int* receiveBorderInfo;
+	std::vector<int> tempReceiveBorderInfo;
+
+
+	double** blockBorder;
+	int* blockBorderMemoryAllocType;
+	std::vector<double*> tempBlockBorder;
+	std::vector<int> tempBlockBorderMemoryAllocType;
+
+
+	double** externalBorder;
+	int* externalBorderMemoryAllocType;
+	std::vector<double*> tempExternalBorder;
+	std::vector<int> tempExternalBorderMemoryAllocType;
+
+
+	int countSendSegmentBorder;
+	int countReceiveSegmentBorder;
 };
 
 #endif /* SRC_BLOCKS_BLOCK_H_ */
