e09736f3564e963f42561875f30f2b6b5dbf7bc7
Daniil Frolov <devnull@localhost>
2015-10-17 14:15:28 +0300

    prepareBorder

--- src/blocks/realblock.cpp
+++ src/blocks/realblock.cpp
@@ -39,5 +39,49 @@ void RealBlock::prepareArgument(int stage, double timestep) {
 }
 
 void RealBlock::prepareStageData(int stage) {
+	double* source = problem->getCurrentStateStageData(stage);
+	for (int i = 0; i < countSendSegmentBorder; ++i) {
+		double* result = blockBorder[i];
 
+		int index = INTERCONNECT_COMPONENT_COUNT * i;
+
+		/*double* source = NULL;
+		cout << endl << "Source = NULL" << endl;*/
+
+		int mStart = sendBorderInfo[ index + M_OFFSET ];
+		int mStop = mStart + sendBorderInfo[ index + M_LENGTH ];
+
+		int nStart = sendBorderInfo[ index + N_OFFSET ];
+		int nStop = nStart + sendBorderInfo[ index + N_LENGTH ];
+		//cout<<"This is block "<<blockNumber<<"preparing data to send: "<< mStart<<" "<<mStop<<" "<<nStart<<" "<<nStop<<endl;
+		//cout<< "side is "<<sendBorderInfo[index + SIDE]<<endl;
+		switch (sendBorderInfo[index + SIDE]) {
+			case LEFT:
+				//prepareBorder(i, stage, mStart, mStop, nStart, nStop, 0, haloSize);
+				pu->prepareBorder(result, source, mStart, mStop, nStart, nStop, 0, haloSize, yCount, xCount, cellSize);
+				break;
+			case RIGHT:
+				//prepareBorder(i, stage, mStart, mStop, nStart, nStop, xCount - haloSize, xCount);
+				pu->prepareBorder(result, source, mStart, mStop, nStart, nStop, xCount - haloSize, xCount, yCount, xCount, cellSize);
+				break;
+			case FRONT:
+				//prepareBorder(i, stage, mStart, mStop, 0, haloSize, nStart, nStop);
+				pu->prepareBorder(result, source, mStart, mStop, 0, haloSize, nStart, nStop, yCount, xCount, cellSize);
+				break;
+			case BACK:
+				//prepareBorder(i, stage, mStart, mStop, yCount - haloSize, yCount, nStart, nStop);
+				pu->prepareBorder(result, source, mStart, mStop, yCount - haloSize, yCount, nStart, nStop, yCount, xCount, cellSize);
+				break;
+			case TOP:
+				//prepareBorder(i, stage, 0, haloSize, mStart, mStop, nStart, nStop);
+				pu->prepareBorder(result, source, 0, haloSize, mStart, mStop, nStart, nStop, yCount, xCount, cellSize);
+				break;
+			case BOTTOM:
+				//prepareBorder(i, stage, zCount - haloSize, zCount, mStart, mStop, nStart, nStop);
+				pu->prepareBorder(result, source, zCount - haloSize, zCount, mStart, mStop, nStart, nStop, yCount, xCount, cellSize);
+				break;
+			default:
+				break;
+		}
+	}
 }
