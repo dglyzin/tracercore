ac1cb3b40e930e277434ff21cd87bcf910b743de
Daniil Frolov <devnull@localhost>
2015-11-05 15:01:38 +0300

    ***

--- src/blocks/nullblock.cpp
+++ src/blocks/nullblock.cpp
@@ -9,12 +9,9 @@
 
 NullBlock::NullBlock(int _nodeNumber, int _dimension, int _xCount, int _yCount, int _zCount, int _xOffset, int _yOffset, int _zOffset, int _cellSize, int _haloSize) :
 		Block(_nodeNumber, _dimension, _xCount, _yCount, _zCount, _xOffset, _yOffset, _zOffset, _cellSize, _haloSize){
-	// TODO Auto-generated constructor stub
-
 }
 
 NullBlock::~NullBlock() {
-	// TODO Auto-generated destructor stub
 }
 
 void NullBlock::computeStageBorder(int stage, double time) {
