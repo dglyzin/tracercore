d46d1bbfa582f0059272cf9df457c5734c362ebe
Daniil Frolov <devnull@localhost>
2015-11-10 14:53:57 +0300

    little fix

--- src/enums.h
+++ src/enums.h
@@ -44,7 +44,7 @@ enum BLOCK_TYPE { NULL_BLOCK, CPU, GPU };
 
 enum PROCESSING_UNIT_TYPE { CPU_UNIT, GPU_UNIT, NOT_UNIT };
 
-enum PROBLEM_TYPE {ORDINARY, DELAY}
+enum PROBLEM_TYPE {ORDINARY, DELAY};
 
 /*
  * Способ выделения памяти.
