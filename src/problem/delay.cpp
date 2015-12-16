cf57ed5004b1973b6e82872f0f83900fd4270afe
Daniil Frolov <devnull@localhost>
2015-12-16 16:28:17 +0300

    add delay problem

---
+++ src/problem/delay.cpp
@@ -0,0 +1,18 @@
+/*
+ * delay.cpp
+ *
+ *  Created on: 16 дек. 2015 г.
+ *      Author: frolov
+ */
+
+#include "delay.h"
+
+Delay::Delay() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Delay::~Delay() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/problem/delay.h
@@ -0,0 +1,19 @@
+/*
+ * delay.h
+ *
+ *  Created on: 16 дек. 2015 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROBLEM_DELAY_H_
+#define SRC_PROBLEM_DELAY_H_
+
+#include "problemtype.h"
+
+class Delay: public ProblemType {
+public:
+	Delay();
+	virtual ~Delay();
+};
+
+#endif /* SRC_PROBLEM_DELAY_H_ */
