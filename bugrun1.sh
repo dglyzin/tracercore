b88b1328c2cade6a2388aede47536f510f5591f9
Daniil Frolov <devnull@localhost>
2015-12-22 17:01:51 +0300

    удаление лишнего файла

--- bugrun1.sh
+++
@@ -1,6 +0,0 @@
-rm res_2400
-rm stat_2400
-python run.py srun 2 test_2400x2400_8 res_2400 -step 1000 -stat stat_2400
-python run.py srun 3 test_2400x2400_8 res_2400 -step 1000 -stat stat_2400 
-python run.py srun 2 test_2400x2400_8 res_2400   -stat stat_2400
-python run.py srun 3 test_2400x2400_8 res_2400   -stat stat_2400
