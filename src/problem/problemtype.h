059c3f9654eaafb941b5207c8920c3561f1f08af
Daniil Frolov <devnull@localhost>
2016-02-03 16:08:08 +0300

    Изменение типа вовращаемого значения для двух новых функций (см. предыдущий коммит)

--- src/problem/problemtype.h
+++ src/problem/problemtype.h
@@ -36,8 +36,8 @@ public:
 
 	virtual double* getCurrentStatePointer() = 0;
 
-	virtual double* saveState(char* path) = 0;
-	virtual double* loadState(char* path) = 0;
+	virtual void saveState(char* path) = 0;
+	virtual void loadState(char* path) = 0;
 
 protected:
 	StepStorage* createStageStorage(ProcessingUnit* pu, int solverType, int count, double aTol, double rTol);
