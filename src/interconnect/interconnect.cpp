7b1725c1248954005bc5fb6ad25b1f15fb82d760
Daniil Frolov <devnull@localhost>
2016-02-19 13:45:11 +0300

    "Отключение" старых функций в i nterconnect.cpp

--- src/interconnect/interconnect.cpp
+++ src/interconnect/interconnect.cpp
@@ -17,44 +17,44 @@ Interconnect::Interconnect(int _sourceLocationNode, int _destinationLocationNode
 Interconnect::~Interconnect() {
 }
 
-void Interconnect::sendRecv(int locationNode) {
-	/*
+/*void Interconnect::sendRecv(int locationNode) {
+
 	 * Пересылка внутри блока.
-	 */
+
 	if(locationNode == sourceLocationNode && locationNode == destinationLocationNode)
 		return;
 
-	/*
+
 	 * Если эту пеерсылку вызвал поток, который содержить информацию для пересылки.
 	 * Фактически этот поток РЕАЛЬНО содержить блок имеющий исходную информацию. Блок-источник.
-	 */
+
 	if(locationNode == sourceLocationNode) {
 		MPI_Isend(sourceBlockBorder, borderLength, MPI_DOUBLE, destinationLocationNode, 999, *mpWorkerComm, request);
 		flag = true;
 		return;
 	}
 
-	/*
+
 	 * Есои пересылку вызвол поток, который должен принимать инфомацию от другого потока.
 	 * Это поток РЕАЛЬНО имеет блок, которому необходима информация от другого блока.
-	 */
+
 	if(locationNode == destinationLocationNode) {
 		MPI_Irecv(destinationExternalBorder, borderLength, MPI_DOUBLE, sourceLocationNode, 999, *mpWorkerComm, request);
 		flag = true;
 		return;
 	}
 
-	/*
-	 * Если же это соединения вызовет поток, которые фактически не имет к нему отношения, то ничего не произойдет.
-	 */
-}
 
-void Interconnect::wait() {
+	 * Если же это соединения вызовет поток, которые фактически не имет к нему отношения, то ничего не произойдет.
+
+}*/
+
+/*void Interconnect::wait() {
 	if( flag ) {
 		MPI_Wait(request, status);
 		flag = false;
 	}
-}
+}*/
 
 void Interconnect::print() {
 	/*cout << endl;
@@ -65,10 +65,10 @@ void Interconnect::print() {
 	cout << "	Destination memory address: " << destinationExternalBorder << endl;
 	cout << endl;*/
 
-	printf("\nInterconnect\n"
+	/*printf("\nInterconnect\n"
 			"	Source node               : %d\n"
 			"	Destination node          : %d\n"
 			"	Source memory address     : %p\n"
 			"	Destination memory address: %p\n"
-			"\n", sourceLocationNode, destinationLocationNode, sourceBlockBorder, destinationExternalBorder);
+			"\n", sourceLocationNode, destinationLocationNode, sourceBlockBorder, destinationExternalBorder);*/
 }
