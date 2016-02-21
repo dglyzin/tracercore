217b9812914138c353752f93c14f49af88e264ec
Daniil Frolov <devnull@localhost>
2016-02-21 15:55:45 +0300

    Изменение реализации функции для поиска последнего вхождения символа в строку

--- src/utils.cpp
+++ src/utils.cpp
@@ -16,8 +16,8 @@ Utils::~Utils() {
 	// TODO Auto-generated destructor stub
 }
 
-int Utils::lastChar(const char* source, char ch) {
-	int i = 0;
+int Utils::lastChar(const char* source, char ch, int num) {
+	/*int i = 0;
 	int index = 0;
 	while (source[i] != 0) {
 		if (source[i] == ch)
@@ -25,13 +25,37 @@ int Utils::lastChar(const char* source, char ch) {
 		i++;
 	}
 
-	return index;
+	return index;*/
+
+	int number = 0;
+
+	int length = strlen(source);
+
+	for (int i = length; i >= 0; --i) {
+		if( source[i] == ch )
+			if( ++number == num )
+				return i;
+	}
+
+	return -1;
 }
 
-void Utils::cutToLastChar(char* result, const char* source, char ch) {
-	int length = Utils::lastChar(source, ch);
+void Utils::copyToLastChar(char* result, const char* source, char ch) {
+	int length = Utils::lastChar(source, ch) + 1;
 
 	strncpy(result, source, length);
 
 	result[length] = 0;
+}
+
+void Utils::cutToLastButOneChar(char* result, const char* source, char ch) {
+	copyToLastChar(result, source, ch);
+	copyToLastChar(result, result, ch);
+}
+
+void Utils::cutFromLastToEnd(char* result, const char* source, char ch) {
+	int pos = Utils::lastChar(source, ch);
+	int length = strlen(source);
+
+	strncpy(result, source+pos, length - pos);
 }
