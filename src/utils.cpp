358da4ab072689182f1405c91ebfc21ef3f5e0d4
Daniil Frolov <devnull@localhost>
2016-02-21 14:39:31 +0300

    Добавлен класс Utils для различных мелких функций, не имеющих прямого отношения к задаче

---
+++ src/utils.cpp
@@ -0,0 +1,29 @@
+/*
+ * utils.cpp
+ *
+ *  Created on: 21 февр. 2016 г.
+ *      Author: frolov
+ */
+
+#include "utils.h"
+
+Utils::Utils() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Utils::~Utils() {
+	// TODO Auto-generated destructor stub
+}
+
+int Utils::lastChar(char* source, char ch) {
+	int i = 0;
+	int index = 0;
+	while (source[i] != 0) {
+		if (source[i] == ch)
+			index = i;
+		i++;
+	}
+
+	return index;
+}
---
+++ src/utils.h
@@ -0,0 +1,19 @@
+/*
+ * utils.h
+ *
+ *  Created on: 21 февр. 2016 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_UTILS_H_
+#define SRC_UTILS_H_
+
+class Utils {
+public:
+	Utils();
+	virtual ~Utils();
+
+	static int lastChar(char* source, char ch);
+};
+
+#endif /* SRC_UTILS_H_ */
