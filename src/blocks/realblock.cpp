7d1ef59dab89a4a897101e584173a20924f4e0bf
Daniil Frolov <devnull@localhost>
2016-03-04 17:20:03 +0300

    RealBlock clean

--- src/blocks/realblock.cpp
+++ src/blocks/realblock.cpp
@@ -18,8 +18,6 @@ RealBlock::RealBlock(int _nodeNumber, int _dimension, int _xCount, int _yCount, 
 
 	blockNumber = _blockNumber;
 
-	/*int elementCount = getGridElementCount();
-	 problem = new Ordinary(pu, solverType, elementCount, aTol, rTol);*/
 	problem = createProblem(problemType, solverType, aTol, rTol);
 
 	sendBorderInfo = NULL;
@@ -52,47 +50,25 @@ RealBlock::RealBlock(int _nodeNumber, int _dimension, int _xCount, int _yCount, 
 
 	double* state = problem->getCurrentStatePointer();
 	pu->initState(state, mUserInitFuncs, mInitFuncNumber, blockNumber, 0.0);
-	//mUserInitFuncs[blockNumber](matrix, mInitFuncNumber);
 }
 
 RealBlock::~RealBlock() {
-	// TODO Auto-generated destructor stub
 }
 
 double* RealBlock::getNewBlockBorder(Block* neighbor, int borderLength) {
-	//double* tmpBorder;
-
 	if ((nodeNumber == neighbor->getNodeNumber()) && neighbor->isProcessingUnitGPU()) {
-		//cudaMallocHost ( (void**)&tmpBorder, borderLength * sizeof(double) );
-		//tempBlockBorderMemoryAllocType.push_back(CUDA_MALLOC_HOST);
-		//memoryType = CUDA_MALLOC_HOST;
 		return pu->newDoublePinnedArray(borderLength);
 	} else {
-		//tmpBorder = new double [borderLength];
-		//tempBlockBorderMemoryAllocType.push_back(NEW);
-		//memoryType = NEW;
 		return pu->newDoubleArray(borderLength);
 	}
-
-	//return tmpBorder;
 }
 
 double* RealBlock::getNewExternalBorder(Block* neighbor, int borderLength, double* border) {
-	//double* tmpBorder;
-
 	if (nodeNumber == neighbor->getNodeNumber()) {
-		//tmpBorder = border;
-		//tempExternalBorderMemoryAllocType.push_back(NOT_ALLOC);
-		//memoryType = NOT_ALLOC;
 		return border;
 	} else {
-		//tmpBorder = new double [borderLength];
-		//tempExternalBorderMemoryAllocType.push_back(NEW);
-		//memoryType = NEW;
 		return pu->newDoubleArray(borderLength);
 	}
-
-	//return tmpBorder;
 }
 
 ProblemType* RealBlock::createProblem(int problemType, int solverType, double aTol, double rTol) {
@@ -115,8 +91,6 @@ void RealBlock::computeStageBorder(int stage, double time) {
 	double* result = problem->getResult(stage);
 	double** source = problem->getSource(stage);
 
-	//printf("\nsource must be double**. => source - &source. Error here\n");
-	//TODO исправить в ProblemType тип возвращаемого значения для getSource
 	pu->computeBorder(mUserFuncs, mCompFuncNumber, result, source, time, mParams, externalBorder, zCount, yCount,
 			xCount, haloSize);
 }
@@ -125,8 +99,6 @@ void RealBlock::computeStageCenter(int stage, double time) {
 	double* result = problem->getResult(stage);
 	double** source = problem->getSource(stage);
 
-	//printf("\nsource must be double**. => source - &source. Error here\n");
-	//TODO исправить в ProblemType тип возвращаемого значения для getSource
 	pu->computeCenter(mUserFuncs, mCompFuncNumber, result, source, time, mParams, externalBorder, zCount, yCount,
 			xCount, haloSize);
 }
@@ -142,41 +114,30 @@ void RealBlock::prepareStageData(int stage) {
 
 		int index = INTERCONNECT_COMPONENT_COUNT * i;
 
-		/*double* source = NULL;
-		 cout << endl << "Source = NULL" << endl;*/
-
 		int mStart = sendBorderInfo[index + M_OFFSET];
 		int mStop = mStart + sendBorderInfo[index + M_LENGTH];
 
 		int nStart = sendBorderInfo[index + N_OFFSET];
 		int nStop = nStart + sendBorderInfo[index + N_LENGTH];
-		//cout<<"This is block "<<blockNumber<<"preparing data to send: "<< mStart<<" "<<mStop<<" "<<nStart<<" "<<nStop<<endl;
-		//cout<< "side is "<<sendBorderInfo[index + SIDE]<<endl;
+
 		switch (sendBorderInfo[index + SIDE]) {
 			case LEFT:
-				//prepareBorder(i, stage, mStart, mStop, nStart, nStop, 0, haloSize);
 				pu->prepareBorder(result, source, mStart, mStop, nStart, nStop, 0, haloSize, yCount, xCount, cellSize);
 				break;
 			case RIGHT:
-				//prepareBorder(i, stage, mStart, mStop, nStart, nStop, xCount - haloSize, xCount);
 				pu->prepareBorder(result, source, mStart, mStop, nStart, nStop, xCount - haloSize, xCount, yCount,
 						xCount, cellSize);
 				break;
 			case FRONT:
-				//prepareBorder(i, stage, mStart, mStop, 0, haloSize, nStart, nStop);
 				pu->prepareBorder(result, source, mStart, mStop, 0, haloSize, nStart, nStop, yCount, xCount, cellSize);
 				break;
 			case BACK:
-				//prepareBorder(i, stage, mStart, mStop, yCount - haloSize, yCount, nStart, nStop);
 				pu->prepareBorder(result, source, mStart, mStop, yCount - haloSize, yCount, nStart, nStop, yCount,
 						xCount, cellSize);
 				break;
-			case TOP:
-				//prepareBorder(i, stage, 0, haloSize, mStart, mStop, nStart, nStop);
 				pu->prepareBorder(result, source, 0, haloSize, mStart, mStop, nStart, nStop, yCount, xCount, cellSize);
 				break;
 			case BOTTOM:
-				//prepareBorder(i, stage, zCount - haloSize, zCount, mStart, mStop, nStart, nStop);
 				pu->prepareBorder(result, source, zCount - haloSize, zCount, mStart, mStop, nStart, nStop, yCount,
 						xCount, cellSize);
 				break;
@@ -231,18 +192,7 @@ double* RealBlock::addNewBlockBorder(Block* neighbor, int side, int mOffset, int
 
 	double* newBlockBorder = getNewBlockBorder(neighbor, borderLength);
 
-	/*if( ( nodeNumber == neighbor->getNodeNumber() ) && isGPU( neighbor->getBlockType() ) ) {
-	 cudaMallocHost ( (void**)&newBlockBorder, borderLength * sizeof(double) );
-	 tempBlockBorderMemoryAllocType.push_back(CUDA_MALLOC_HOST);
-	 }
-	 else {
-	 newBlockBorder = new double [borderLength];
-	 tempBlockBorderMemoryAllocType.push_back(NEW);
-	 }*/
-
 	tempBlockBorder.push_back(newBlockBorder);
-
-	//printf("\nBorder %d %d %d\n", nodeNumber, blockNumber, newBlockBorder);
 
 	return newBlockBorder;
 }
@@ -261,15 +211,6 @@ double* RealBlock::addNewExternalBorder(Block* neighbor, int side, int mOffset, 
 
 	double* newExternalBorder = getNewExternalBorder(neighbor, borderLength, border);
 
-	/*if( nodeNumber == neighbor->getNodeNumber() ) {
-	 newExternalBorder = border;
-	 tempExternalBorderMemoryAllocType.push_back(NOT_ALLOC);
-	 }
-	 else {
-	 newExternalBorder = new double [borderLength];
-	 tempExternalBorderMemoryAllocType.push_back(NEW);
-	 }*/
-
 	tempExternalBorder.push_back(newExternalBorder);
 
 	//printf("\nExternal %d %d %d\n", nodeNumber, blockNumber, newExternalBorder);
@@ -278,17 +219,16 @@ double* RealBlock::addNewExternalBorder(Block* neighbor, int side, int mOffset, 
 }
 
 void RealBlock::moveTempBorderVectorToBorderArray() {
-	blockBorder = pu->newDoublePointerArray(countSendSegmentBorder);		//new double* [countSendSegmentBorder];
-	//blockBorderMemoryAllocType = new int [countSendSegmentBorder];
-	sendBorderInfo = pu->newIntArray(INTERCONNECT_COMPONENT_COUNT * countSendSegmentBorder);//new int [INTERCONNECT_COMPONENT_COUNT * countSendSegmentBorder];
+	blockBorder = pu->newDoublePointerArray(countSendSegmentBorder);
 
-	externalBorder = pu->newDoublePointerArray(countReceiveSegmentBorder);	//new double* [countReceiveSegmentBorder];
-	//externalBorderMemoryAllocType = new int [countReceiveSegmentBorder];
-	receiveBorderInfo = pu->newIntArray(INTERCONNECT_COMPONENT_COUNT * countReceiveSegmentBorder);//new int [INTERCONNECT_COMPONENT_COUNT * countReceiveSegmentBorder];
+	sendBorderInfo = pu->newIntArray(INTERCONNECT_COMPONENT_COUNT * countSendSegmentBorder);
+
+	externalBorder = pu->newDoublePointerArray(countReceiveSegmentBorder);
+
+	receiveBorderInfo = pu->newIntArray(INTERCONNECT_COMPONENT_COUNT * countReceiveSegmentBorder);
 
 	for (int i = 0; i < countSendSegmentBorder; ++i) {
 		blockBorder[i] = tempBlockBorder.at(i);
-		//blockBorderMemoryAllocType[i] = tempBlockBorderMemoryAllocType.at(i);
 
 		int index = INTERCONNECT_COMPONENT_COUNT * i;
 		sendBorderInfo[index + SIDE] = tempSendBorderInfo.at(index + 0);
@@ -300,7 +240,6 @@ void RealBlock::moveTempBorderVectorToBorderArray() {
 
 	for (int i = 0; i < countReceiveSegmentBorder; ++i) {
 		externalBorder[i] = tempExternalBorder.at(i);
-		//externalBorderMemoryAllocType[i] = tempExternalBorderMemoryAllocType.at(i);
 
 		int index = INTERCONNECT_COMPONENT_COUNT * i;
 		receiveBorderInfo[index + SIDE] = tempReceiveBorderInfo.at(index + 0);
@@ -312,9 +251,6 @@ void RealBlock::moveTempBorderVectorToBorderArray() {
 
 	tempBlockBorder.clear();
 	tempExternalBorder.clear();
-
-	//tempBlockBorderMemoryAllocType.clear();
-	//tempExternalBorderMemoryAllocType.clear();
 
 	tempSendBorderInfo.clear();
 	tempReceiveBorderInfo.clear();
