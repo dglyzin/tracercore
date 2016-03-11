5f29c3d6a39231671b8b2512f629e9f01f9d1ff9
Daniil Frolov <devnull@localhost>
2016-03-11 10:08:21 +0300

    some clean

--- src/processingunit/cpu/cpu.h
+++ src/processingunit/cpu/cpu.h
@@ -18,9 +18,6 @@ public:
 	CPU(int _deviceNumber);
 	virtual ~CPU();
 
-	//virtual void computeBorder(double* result, double** source, double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) = 0;
-	//virtual void computeCenter(double* result, double** source, double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) = 0;
-
 	void prepareBorder(double* result, double* source, int zStart, int zStop, int yStart, int yStop, int xStart,
 			int xStop, int yCount, int xCount, int cellSize);
 
