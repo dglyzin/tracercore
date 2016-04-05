144fbd7e053a14388b788bd9e1e119a12abffc98
Daniil Frolov <devnull@localhost>
2016-04-05 15:33:15 +0300

    add GPU_1D

---
+++ src/processingunit/gpu/gpu1d.cpp
@@ -0,0 +1,16 @@
+/*
+ * gpu1d.cpp
+ *
+ *  Created on: 05 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#include "gpu1d.h"
+
+GPU_1D::GPU_1D(int _deviceNumber) :
+		GPU(_deviceNumber) {
+}
+
+GPU_1D::~GPU_1D() {
+}
+
---
+++ src/processingunit/gpu/gpu1d.h
@@ -0,0 +1,26 @@
+/*
+ * gpu1d.h
+ *
+ *  Created on: 05 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_GPU_GPU1D_H_
+#define SRC_PROCESSINGUNIT_GPU_GPU1D_H_
+
+#include "gpu.h"
+
+class GPU_1D: public GPU {
+public:
+	GPU_1D(int _deviceNumber);
+	virtual ~GPU_1D();
+
+	void computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+	void computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+
+	void printArray(double* array, int zCount, int yCount, int xCount, int cellSize);
+};
+
+#endif /* SRC_PROCESSINGUNIT_GPU_GPU1D_H_ */
