9622425e7d8215b26afbfd0276cb661f918551e7
Daniil Frolov <devnull@localhost>
2016-04-05 15:36:27 +0300

    Заглушки для расчетов и вывода на консоль для GPU_1d

--- src/processingunit/gpu/gpu1d.cpp
+++ src/processingunit/gpu/gpu1d.cpp
@@ -14,3 +14,38 @@ GPU_1d::GPU_1d(int _deviceNumber) :
 GPU_1d::~GPU_1d() {
 }
 
+void GPU_1d::computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+/*# pragma omp parallel
+	{
+# pragma omp for
+		for (int x = 0; x < haloSize; ++x) {
+			//cout << "Border Calc x_" << x << endl;
+			mUserFuncs[mCompFuncNumber[x]](result, source, time, x, 0, 0, parametrs, externalBorder);
+		}
+
+# pragma omp for
+		for (int x = xCount - haloSize; x < xCount; ++x) {
+			//cout << "Border Calc x_" << x << endl;
+			mUserFuncs[mCompFuncNumber[x]](result, source, time, x, 0, 0, parametrs, externalBorder);
+		}
+	}*/
+
+	//scanf("%c", &c);
+}
+
+void GPU_1d::computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+/*# pragma omp parallel
+	{
+# pragma omp for
+		for (int x = haloSize; x < xCount - haloSize; ++x) {
+			//cout << "Calc x_" << x << endl;
+			mUserFuncs[mCompFuncNumber[x]](result, source, time, x, 0, 0, parametrs, externalBorder);
+		}
+	}*/
+}
+
+void GPU_1d::printArray(double* array, int zCount, int yCount, int xCount, int cellSize) {
+	//printArray1d(array, xCount, cellSize);
+}
