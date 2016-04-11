be3103fcbdf6e67b50d81365add85985c4c068d5
Daniil Frolov <devnull@localhost>
2016-04-11 15:50:10 +0300

    Заглушки в классе + отладочный вывод

--- src/processingunit/gpu/gpu2d.cpp
+++ src/processingunit/gpu/gpu2d.cpp
@@ -7,12 +7,31 @@
 
 #include "gpu2d.h"
 
-GPU_2d::GPU_2d() {
-	// TODO Auto-generated constructor stub
-
+GPU_2d::GPU_2d(int _deviceNumber) :
+		GPU(_deviceNumber) {
 }
 
 GPU_2d::~GPU_2d() {
-	// TODO Auto-generated destructor stub
 }
 
+void GPU_2d::computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+	computeBorderGPU_2d();
+}
+
+void GPU_2d::computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+	computeCenterGPU_2d();
+}
+
+void GPU_2d::printArray(double* array, int zCount, int yCount, int xCount, int cellSize) {
+	int size = zCount * yCount * xCount * cellSize;
+
+	double* tmpArray = new double[size];
+
+	cudaMemcpy(array, tmpArray, size * sizeof(double), cudaMemcpyDeviceToHost);
+
+	printArray2d(array, xCount, yCount, cellSize);
+
+	delete tmpArray;
+}
--- src/processingunit/gpu/gpu2d.h
+++ src/processingunit/gpu/gpu2d.h
@@ -12,8 +12,15 @@
 
 class GPU_2d: public GPU {
 public:
-	GPU_2d();
+	GPU_2d(int _deviceNumber);
 	virtual ~GPU_2d();
+
+	void computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+	void computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+
+	void printArray(double* array, int zCount, int yCount, int xCount, int cellSize);
 };
 
 #endif /* SRC_PROCESSINGUNIT_GPU_GPU2D_H_ */
