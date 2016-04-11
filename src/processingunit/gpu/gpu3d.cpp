481e73e521ac12497f3f5ee44da6d73712ec8ac4
Daniil Frolov <devnull@localhost>
2016-04-11 17:28:09 +0300

    add gpu3d

---
+++ src/processingunit/gpu/gpu3d.cpp
@@ -0,0 +1,18 @@
+/*
+ * gpu3d.cpp
+ *
+ *  Created on: 11 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#include "gpu3d.h"
+
+GPU_3d::GPU_3d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPU_3d::~GPU_3d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingunit/gpu/gpu3d.h
@@ -0,0 +1,19 @@
+/*
+ * gpu3d.h
+ *
+ *  Created on: 11 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_GPU_GPU3D_H_
+#define SRC_PROCESSINGUNIT_GPU_GPU3D_H_
+
+#include "gpu.h"
+
+class GPU_3d: public GPU {
+public:
+	GPU_3d();
+	virtual ~GPU_3d();
+};
+
+#endif /* SRC_PROCESSINGUNIT_GPU_GPU3D_H_ */
