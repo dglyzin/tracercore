3e1b47383f0152d12e0f95c92a9e6b98a436961a
Daniil Frolov <devnull@localhost>
2016-04-11 15:42:37 +0300

    add GPU_2d

---
+++ src/processingunit/gpu/gpu2d.cpp
@@ -0,0 +1,18 @@
+/*
+ * gpu2d.cpp
+ *
+ *  Created on: 11 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#include "gpu2d.h"
+
+GPU_2d::GPU_2d() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GPU_2d::~GPU_2d() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/processingunit/gpu/gpu2d.h
@@ -0,0 +1,19 @@
+/*
+ * gpu2d.h
+ *
+ *  Created on: 11 апр. 2016 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROCESSINGUNIT_GPU_GPU2D_H_
+#define SRC_PROCESSINGUNIT_GPU_GPU2D_H_
+
+#include "gpu.h"
+
+class GPU_2d: public GPU {
+public:
+	GPU_2d();
+	virtual ~GPU_2d();
+};
+
+#endif /* SRC_PROCESSINGUNIT_GPU_GPU2D_H_ */
