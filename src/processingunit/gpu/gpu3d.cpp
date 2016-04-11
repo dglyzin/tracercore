793b7430bac3e162ca7a05c3ff8d9d10aa1f9ecc
Daniil Frolov <devnull@localhost>
2016-04-11 17:31:36 +0300

    Заглушки + вывод в классе GPU_3d

--- src/processingunit/gpu/gpu3d.cpp
+++ src/processingunit/gpu/gpu3d.cpp
@@ -14,3 +14,24 @@ GPU_3d::GPU_3d(int _deviceNumber) :
 GPU_3d::~GPU_3d() {
 }
 
+void GPU_3d::computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+	computeBorderGPU_3d();
+}
+
+void GPU_3d::computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+		double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize) {
+	computeCenterGPU_3d();
+}
+
+void GPU_3d::printArray(double* array, int zCount, int yCount, int xCount, int cellSize) {
+	int size = zCount * yCount * xCount * cellSize;
+
+	double* tmpArray = new double[size];
+
+	cudaMemcpy(array, tmpArray, size * sizeof(double), cudaMemcpyDeviceToHost);
+
+	printArray3d(array, xCount, yCount, zCount, cellSize);
+
+	delete tmpArray;
+}
--- src/processingunit/gpu/gpu3d.h
+++ src/processingunit/gpu/gpu3d.h
@@ -14,6 +14,13 @@ class GPU_3d: public GPU {
 public:
 	GPU_3d(int _deviceNumber);
 	virtual ~GPU_3d();
+
+	void computeBorder(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+	void computeCenter(func_ptr_t* mUserFuncs, unsigned short int* mCompFuncNumber, double* result, double** source,
+			double time, double* parametrs, double** externalBorder, int zCount, int yCount, int xCount, int haloSize);
+
+	void printArray(double* array, int zCount, int yCount, int xCount, int cellSize);
 };
 
 #endif /* SRC_PROCESSINGUNIT_GPU_GPU3D_H_ */
