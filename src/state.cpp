fafedac351f7832812e9fd67b5b85569880f4b75
Daniil Frolov <devnull@localhost>
2016-10-10 17:27:50 +0300

    add new class State

---
+++ src/state.cpp
@@ -0,0 +1,18 @@
+/*
+ * state.cpp
+ *
+ *  Created on: 10 окт. 2016 г.
+ *      Author: frolov
+ */
+
+#include "state.h"
+
+State::State() {
+	// TODO Auto-generated constructor stub
+
+}
+
+State::~State() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/state.h
@@ -0,0 +1,17 @@
+/*
+ * state.h
+ *
+ *  Created on: 10 окт. 2016 г.
+ *      Author: frolov
+ */
+
+#ifndef STATE_H_
+#define STATE_H_
+
+class State {
+public:
+	State();
+	virtual ~State();
+};
+
+#endif /* STATE_H_ */
