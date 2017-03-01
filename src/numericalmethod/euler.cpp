54136e0db85e9f1436fa5629bdec4c9ff6738317
Frolov Daniil <devnull@localhost>
2017-03-01 12:09:47 +0300

    Добавлен метод Эйлера

---
+++ src/numericalmethod/euler.cpp
@@ -0,0 +1,92 @@
+/*
+ * euler.cpp
+ *
+ *  Created on: 13 февр. 2017 г.
+ *      Author: frolov
+ */
+
+#include "euler.h"
+
+Euler::Euler(double _aTol, double _rTol) :
+		NumericalMethod(_aTol, _rTol) {
+}
+
+Euler::~Euler() {
+}
+
+int Euler::getStageCount() {
+	return 1;
+}
+
+bool Euler::isFSAL() {
+	return false;
+}
+
+bool Euler::isErrorPermissible(double error, int totalDomainElements) {
+	return true;
+}
+
+bool Euler::isVariableStep() {
+	return false;
+}
+
+double Euler::computeNewStep(double timeStep, double error, int totalDomainElements) {
+	return timeStep;
+}
+
+int Euler::getMemorySizePerState(int elementCount) {
+	// TODO: 2 заменить на что-то более вразумительное
+	return (elementCount * SIZE_DOUBLE) * (1 + KSTORAGE_COUNT);
+}
+
+int Euler::getKStorageCount() {
+	return KSTORAGE_COUNT;
+}
+
+int Euler::getCommonTempStorageCount() {
+	return 0;
+}
+
+double* Euler::getStorageResult(double* state, double** kStorages, double** commonTempStorages, int stageNumber) {
+	return kStorages[K1];
+}
+
+double* Euler::getStorageSource(double* state, double** kStorages, double** commonTempStorages, int stageNumber) {
+	return state;
+}
+
+double Euler::getStageTimeStepCoefficient(int stageNumber) {
+	// TODO: точно?
+	return 0.0;
+}
+
+void Euler::prepareArgument(ProcessingUnit* pu, double* state, double** kStorages, double** commonTempStorages,
+		double timeStep, int stageNumber, int size) {
+	return;
+}
+
+void Euler::confirmStep(ProcessingUnit* pu, ISmartCopy* sc, double* sourceState, double** sourceKStorages,
+		double* destinationState, double** destinationKStorages, double** commonTemp, double timeStep, int size) {
+	/*pu->multiplyArrayByNumber(mTempStore1, mTempStore1, timestep, mCount);
+	 pu->sumArrays(mTempStore1, mState, mTempStore1, mCount);
+
+	 double* temp = mState;
+	 mState = mTempStore1;
+	 mTempStore1 = temp;*/
+	pu->multiplyArrayByNumberAndSum(destinationState, sourceKStorages[K1], timeStep, sourceState, size);
+}
+
+void Euler::rejectStep(ProcessingUnit* pu, double* state, double** kStorages, double** commonTempStorages,
+		double timeStep, int size) {
+	return;
+}
+
+double Euler::computeStepError(ProcessingUnit* pu, double** kStorages, double** commonTempStorages, double timeStep,
+		int size) {
+	return 0.0;
+}
+
+void Euler::computeDenseOutput(ProcessingUnit* pu, double* state, double** kStorages, double timeStep, double theta,
+		double* result, int size) {
+
+}
---
+++ src/numericalmethod/euler.h
@@ -0,0 +1,54 @@
+/*
+ * euler.h
+ *
+ *  Created on: 13 февр. 2017 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_NUMERICALMETHOD_EULER_H_
+#define SRC_NUMERICALMETHOD_EULER_H_
+
+#include "numericalmethod.h"
+
+class Euler: public NumericalMethod {
+public:
+	Euler(double _aTol, double _rTol);
+	virtual ~Euler();
+
+	int getStageCount();
+	bool isFSAL();
+	bool isErrorPermissible(double error, int totalDomainElements);
+	bool isVariableStep();
+	double computeNewStep(double timeStep, double error, int totalDomainElements);
+
+	int getMemorySizePerState(int elementCount);
+
+	int getKStorageCount();
+	int getCommonTempStorageCount();
+
+	double* getStorageResult(double* state, double** kStorages, double** commonTempStorages, int stageNumber);
+	double* getStorageSource(double* state, double** kStorages, double** commonTempStorages, int stageNumber);
+
+	double getStageTimeStepCoefficient(int stageNumber);
+
+	void prepareArgument(ProcessingUnit* pu, double* state, double** kStorages, double** commonTempStorages,
+			double timeStep, int stageNumber, int size);
+
+	void confirmStep(ProcessingUnit* pu, ISmartCopy* sc, double* sourceState, double** sourceKStorages,
+			double* destinationState, double** destinationKStorages, double** commonTemp, double timeStep, int size);
+	void rejectStep(ProcessingUnit* pu, double* state, double** kStorages, double** commonTempStorages, double timeStep,
+			int size);
+
+	double computeStepError(ProcessingUnit* pu, double** kStorages, double** commonTempStorages, double timeStep,
+			int size);
+
+	void computeDenseOutput(ProcessingUnit* pu, double* state, double** kStorages, double timeStep, double theta,
+			double* result, int size);
+
+private:
+	enum KSTORAGE {
+		K1, KSTORAGE_COUNT
+	};
+};
+
+#endif /* SRC_NUMERICALMETHOD_EULER_H_ */
