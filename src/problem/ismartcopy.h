772d780910e42e7b0b3416da8da6eef8a855efd9
Frolov Daniil <devnull@localhost>
2017-03-01 12:12:23 +0300

    Интерфейс для корректного копирования массивов (или swap) для разных типов задачи

---
+++ src/problem/ismartcopy.h
@@ -0,0 +1,21 @@
+/*
+ * ismartcopy.h
+ *
+ *  Created on: 27 февр. 2017 г.
+ *      Author: frolov
+ */
+
+#ifndef SRC_PROBLEM_ISMARTCOPY_H_
+#define SRC_PROBLEM_ISMARTCOPY_H_
+
+#include "../processingunit/processingunit.h"
+
+class ISmartCopy {
+public:
+	ISmartCopy();
+	virtual ~ISmartCopy();
+
+	virtual void swapCopy(ProcessingUnit* pu, double** source, double** destination, int size) = 0;
+};
+
+#endif /* SRC_PROBLEM_ISMARTCOPY_H_ */
