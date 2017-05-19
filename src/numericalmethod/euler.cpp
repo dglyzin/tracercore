b38378cf76c1e6671041c32ff3becd5b3e15a36e
Daniil Frolov <devnull@localhost>
2017-05-19 11:41:53 +0300

    Отмена коммита ea8b332

--- src/numericalmethod/euler.cpp
+++ src/numericalmethod/euler.cpp
@@ -52,7 +52,7 @@ double* Euler::getStorageSource(double* state, double** kStorages, double** comm
 
 double Euler::getStageTimeStepCoefficient(int stageNumber) {
 	// TODO: точно?
-	return 1.0;
+	return 0.0;
 }
 
 void Euler::prepareArgument(ProcessingUnit* pu, double* state, double** kStorages, double** commonTempStorages,
